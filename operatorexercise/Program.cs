c132f504f8765eb3837ce7e542275e3ecc4fc23d
Ashley Tucker <ashleyst35@aol.com>
2022-08-09 15:53:36 -0400

    operatorsexercise2

---
+++ operatorexercise/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// exc 1
+    int a = 17;
+    int b = 4;
+
+    int quatient = a / b;
+    int remainder = a % b;
+    if (a == 17 && b == 4)
+   { 
+        Console.WriteLine($"{a} /= {b}is {quatient} remainder of {remainder}");
+    }
+
+
+ 
